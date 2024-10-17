e000fb42058d9b2f7731dcd6e4fddd24ac66ce08
shiyasmt <shiyasmt8576@gmail.com>
2024-10-17 12:58:22 +0530

    Add files via upload

---
+++ day 8.py
@@ -0,0 +1,44 @@
+data1=[5,2,3,6,8]
+type(data1)
+print(data1)
+print(data1[1])
+print(data1[0])
+print(data1[0:2])
+print(data1[::2])
+for value in data1:
+    print(value)
+data2=[25,20,51,62,0,10,20,]
+type(data2)
+for value in data2:
+    print(value)
+students=['salif','basil','fazil','riswan']
+type(students)
+for name in students:
+    print(name)
+dir(students)
+students.insert(0,'shiyas')
+students
+for name in students:
+    print(name)
+for value in range(1,100):
+    print(value)
+for value in range(99,66,-1):
+    print(value)
+for value in range(99999,8888,-1):
+    print(value)
+for value in range(20,60,2):
+    print(value)
+for value in range(5,150,5):
+    print(value)
+for value in range(500,250,-50):
+    print(value)
+for value in range(200000,100000,-10000):
+    print(value)
+for marks in range(20,50,6):
+    print(marks)
+for value in range(0,7):
+    print(data2[value])
+data3=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+type(data3)
+for nombers in range(1,17,2):
+    print(data3[nombers])
