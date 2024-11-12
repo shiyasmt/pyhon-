97e4ff511f811ddd0ff9a706ca09acd50bea6fc6
shiyasmt <shiyasmt8576@gmail.com>
2024-11-12 14:13:33 +0530

    Add files via upload

---
+++ while loop 5.0.py
@@ -0,0 +1,48 @@
+a=int(input("enter a value and 0 for quit"))
+sum=0
+while a!=0:
+    sum=sum+a
+    a=int(input("enter a value and 0 for quit"))
+
+sum
+
+b=int(input("enter a value and sum"))
+sum=0
+while b!=0:
+    sum=sum+b
+    print(sum)
+    b=int(input("enter a value and sum"))
+
+a=int(input("enter a value and sum"))
+sum=0
+while a!=0:
+    sum=sum+a
+    print(sum)
+    a=int(input("enter a value and sum"))
+
+list1=[]
+
+a=input("enter a value and 0 for quit")
+while a<"5":
+    list1.append(a)
+    a=input("enter a value and 0 for quit")
+
+list1
+
+list2=[]
+a=int(input("enter a integer value and appent to list2"))
+while a<7:
+    list2.append(a)
+    print(list2)
+    a=int(input("enter a integer value and appent to list2"))
+
+list2
+
+list3=[]
+
+a=int(input("enter a integer value and appent"))
+sum=0
+while a>10:
+    sum=sum+a
+    print(sum)
+    a=int(input("enter a integer value ans appent"))
