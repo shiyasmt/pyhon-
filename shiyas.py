8e9a005b3bfedc04e8a8e8babd420e56069e27a4
shiyasmt <shiyasmt8576@gmail.com>
2024-11-12 11:50:49 +0530

    Add files via upload

---
+++ shiyas.py
@@ -0,0 +1,36 @@
+a=input("enter a value and 0 for quit")
+while a!="0":
+    print(a)
+    a=input("enter a value and 0 for quit")
+
+marks=[]
+
+a=input("enter a value and appent")
+while a!="0":
+    marks.append(a)
+    a=input("enter a value and appent")
+
+marks
+
+a=input("enter a value")
+while a!="0":
+    marks.remove(a)
+    a=input("enter a value")
+
+marks
+
+a=input("enter a value and 0 for quit")
+while a!="0":
+    marks.append(a)
+    a=input("enter a value and 0 for quit")
+
+mark=[]
+
+a=input("enter a value")
+while True:
+    if a==4:
+        break
+    mark.append(a)
+    a=input("enter a value")
+
+mark
